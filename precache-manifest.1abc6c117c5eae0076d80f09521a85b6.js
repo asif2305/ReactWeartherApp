fc33d0a15d65d048d723b4e891c27ae79cfeaeb8
asif2305 <mygithub2305@gmail.com>
2020-05-05 04:38:54 +0200

    Updates

---
+++ precache-manifest.1abc6c117c5eae0076d80f09521a85b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33de87113bd6e2bd142e8be19f21ac6",
+    "url": "/ReactWeartherApp/index.html"
+  },
+  {
+    "revision": "38295aec244885c8a084",
+    "url": "/ReactWeartherApp/static/css/main.9f18368f.chunk.css"
+  },
+  {
+    "revision": "4356a12677ab2ea2bad1",
+    "url": "/ReactWeartherApp/static/js/2.11f747fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeartherApp/static/js/2.11f747fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38295aec244885c8a084",
+    "url": "/ReactWeartherApp/static/js/main.dd738c54.chunk.js"
+  },
+  {
+    "revision": "beee9e27fa87ed991811",
+    "url": "/ReactWeartherApp/static/js/runtime-main.a1419ce0.js"
+  },
+  {
+    "revision": "075bc39ddef557eaa2050b15e7ea3a5f",
+    "url": "/ReactWeartherApp/static/media/1.075bc39d.png"
+  },
+  {
+    "revision": "17653dcccd5f638e133892401efd3718",
+    "url": "/ReactWeartherApp/static/media/2.17653dcc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactWeartherApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
